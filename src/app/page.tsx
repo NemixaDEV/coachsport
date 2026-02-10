e8c916ce86febf5c7ab9a830f87059fb8d7e478a
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2026-02-10 09:48:07 -0300

    Initial Commit.

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center">
+        <h1 className="text-2xl leading-normal px-7 py-12">Initial Commit.</h1>
+      </main>
+    </div>
+  )
+}
